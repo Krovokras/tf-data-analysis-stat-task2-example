3340e207f1e7416a71ce590e92aa83cc8a480226
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-07 22:25:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import numpy as np
-
 from scipy.stats import norm
 
-
-chat_id = 1134491798 # Ваш chat ID, не меняйте название переменной
+chat_id = 1134491798
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-a, b = 0.068, 6
-    loc = (a + b) / 2
-    scale = (b - a) / np.sqrt(12)
-    alpha = 1 - p
-    return loc - scale * uniform.ppf(1 - alpha / 2), \
-           loc - scale * uniform.ppf(alpha / 2)
+
+alpha = 1 - p
+loc = x.mean()
+scale = np.sqrt(np.var(x))/np.sqrt(len(x))
+
+left_quantile = loc - scale * norm.ppf(1 - alpha / 2)
+right_quantile = loc - scale * norm.ppf(alpha / 2)
+return left_quantile, right_quantile
